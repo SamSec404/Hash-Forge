caacc07e0d370a1b62b43183aa7efe914c7d2e10
Sam <zerodaybypass48@gmail.com>
2026-08-05 16:11:31 +0500

    Update __init__.py

--- Routes/__init__.py
+++ Routes/__init__.py
@@ -1,1 +1,7 @@
-
+from .generate import generate_bp
+from .verify import verify_bp
+from .commpare import compare_bp
+from .file_hash import file_hash_bp
+from .history import history_bp
+from .delete_history import delete_history_bp
+from .clear_history import clear_history_bp
