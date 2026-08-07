644bfccd2d634216a4b83727ef8e03a84bfd9f31
Sam <zerodaybypass48@gmail.com>
2026-08-07 17:17:09 +0500

    Update copy.js

--- Static/JS/copy.js
+++ Static/JS/copy.js
@@ -1,1 +1,19 @@
+function copyHash(textareaId) {
 
+    const textarea = document.getElementById(textareaId);
+
+    if (!textarea) return;
+
+    navigator.clipboard.writeText(textarea.value)
+        .then(() => {
+
+            alert("Hash copied successfully!");
+
+        })
+        .catch(() => {
+
+            alert("Unable to copy hash.");
+
+        });
+
+}
