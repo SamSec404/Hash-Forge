a2ab2a07820af105abbc8cd371d98f862dffbc01
Sam <zerodaybypass48@gmail.com>
2026-08-08 20:21:05 +0500

    Added Utils

---
+++ integrity.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+INTEGRITY_FILE = "models/integrity.json"
+
+
+def load_integrity():
+
+    if not os.path.exists(INTEGRITY_FILE):
+
+        return {}
+
+    with open(INTEGRITY_FILE, "r") as file:
+
+        return json.load(file)
+
+
+def save_integrity(data):
+
+    with open(INTEGRITY_FILE, "w") as file:
+
+        json.dump(data, file, indent=4)
+
+
+def register_file(filename, file_hash):
+
+    data = load_integrity()
+
+    data[filename] = file_hash
+
+    save_integrity(data)
+
+
+def verify_file(filename, new_hash):
+
+    data = load_integrity()
+
+    if filename not in data:
+
+        return "NOT REGISTERED"
+
+    if data[filename] == new_hash:
+
+        return "VERIFIED"
+
+    return "MODIFIED"
