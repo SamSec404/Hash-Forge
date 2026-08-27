c9ffe67d0c1973523180ee496c1c4644555a95e0
Sam <zerodaybypass48@gmail.com>
2026-08-27 15:54:20 +0500

    Added Utils\

--- Models/hash_record.py
+++ Models/hash_record.py
@@ -1,19 +1,14 @@
 from dataclasses import dataclass, asdict
 
-
 @dataclass
 class HashRecord:
-
     record_type: str      # text or file
-
     original_input: str   # text or filename
-
     algorithm: str
-
     generated_hash: str
-
     timestamp: str
 
     def to_dict(self):
-
         return asdict(self)
+
+
