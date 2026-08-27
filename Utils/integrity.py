e6aa4b3e8b89248789f625f7856f329f6f834d01
Sam <zerodaybypass48@gmail.com>
2026-08-27 15:50:13 +0500

    Added Utils

--- Utils/integrity.py
+++ Utils/integrity.py
@@ -3,44 +3,36 @@ import os
 
 INTEGRITY_FILE = "models/integrity.json"
 
-
 def load_integrity():
-
     if not os.path.exists(INTEGRITY_FILE):
-
         return {}
 
     with open(INTEGRITY_FILE, "r") as file:
-
         return json.load(file)
 
 
 def save_integrity(data):
-
     with open(INTEGRITY_FILE, "w") as file:
-
         json.dump(data, file, indent=4)
 
 
 def register_file(filename, file_hash):
-
     data = load_integrity()
-
     data[filename] = file_hash
-
     save_integrity(data)
 
 
 def verify_file(filename, new_hash):
-
     data = load_integrity()
 
     if filename not in data:
-
         return "NOT REGISTERED"
 
     if data[filename] == new_hash:
-
         return "VERIFIED"
 
     return "MODIFIED"
+
+
+
+
