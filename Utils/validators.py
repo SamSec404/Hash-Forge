2eabca3aea67a0e12d18e8ca6da7fd526d2b8c46
Sam <zerodaybypass48@gmail.com>
2026-08-27 15:52:16 +0500

    Added Utils

--- Utils/validators.py
+++ Utils/validators.py
@@ -1,10 +1,11 @@
 from config import ALLOWED_EXTENSIONS
 
-
 def allowed_file(filename):
-
     return (
         "." in filename
         and filename.rsplit(".", 1)[1].lower()
         in ALLOWED_EXTENSIONS
     )
+
+
+
